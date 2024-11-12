2667104c3b22066a22bbc64f76e72c7ca38b0f67
Emran Hossain <emranhossain0335@gmail.com>
2024-11-12 22:19:25 +0600

    arithmatic operations are done suing oparends

---
+++ ArithmaticDemo.java
@@ -0,0 +1,30 @@
+
+package beginnerjava;
+
+
+public class ArithmaticDemo {
+    
+    public static void main(String[] args) {
+        int num1, num2, result;
+
+        num1 = 20;
+        num2 = 10;
+        
+        result = num1 + num2;
+        System.out.println("Sum = "+result);
+        
+        result = num1 - num2;
+        System.out.println("Sub = "+result);
+        
+        result = num1 * num2;
+        System.out.println("Mul = "+result);
+        
+        result = num1 / num2;
+        System.out.println("Div = "+result);
+        
+        result = num1 % num2;
+        System.out.println("Mod = "+result);
+        
+    }
+    
+}
