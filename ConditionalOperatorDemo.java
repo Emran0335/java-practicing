4f7f0b9a4f11854ece4b9cc0d0f98a0e37633c32
emran0335 <emranhossain0335@gmail.com>
2024-12-07 15:28:21 +0600

    Conditional Operator is sued

---
+++ ConditionalOperatorDemo.java
@@ -0,0 +1,21 @@
+package beginnerjava;
+
+import java.util.Scanner;
+
+
+public class ConditionalOperatorDemo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1, num2, large;
+        
+        System.out.print("Enter 2 numbers: ");
+        num1 = input.nextInt();
+        
+        num2 = input.nextInt();
+        
+        large = num1 > num2 ? num1 : num2;
+        
+        System.out.println("The large number: "+large);
+        
+    }
+}
